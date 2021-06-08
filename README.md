4234645274250007ee914b4545256f4fd06ecef7
yhsiao2 <yhsiao2uni@outlook.com>
2021-06-08 17:07:55 +0700

    final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+##About
+This is a project about creating a weather app by pulling data from an API. Which openweathermap.org was used.
+
+##Usage
+Since the web app requires an API key to work, please either input your own API key from openweathermap.org
+
+##References
+This project idea was referenced from Dev Ed's youtube channel. Link: https://www.youtube.com/watch?v=wPElVpR1rwA&ab_channel=DevEd. 
+All edits and configurations are my own.
--- app.js
+++ app.js
@@ -21,39 +21,70 @@ window.addEventListener('load', ()=>
     let humidityMeasure = document.querySelector('.humidity')
     
     
+    let degreeSection = document.querySelector('.degree-section')
+    const degreeSectionSpan = document.querySelector('.degree-section span')
 
+    let degreeFeels = document.querySelector('.degree-feels')
+    const degreeFeelsSpan = document.querySelector('.degree-feels span')
 
     if(navigator.geolocation)
     {
         navigator.geolocation.getCurrentPosition(position =>
-            {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-    
-            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${weatherApiKey}`;
+        {
+                long = position.coords.longitude;
+                lat = position.coords.latitude;
+        
+                const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${weatherApiKey}`;
 
-            fetch(api) //fetches API data 
-            .then(apiResponse => 
-                { //does line 12 once it receives the data
-                return apiResponse.json();
-                }) 
-            .then(apiData =>{
-                console.log(apiData);
-                const {temp, feels_like, humidity}= apiData.main;
-                const {name} = apiData;
-                const{country} = apiData.sys;
-                const {icon, description} = apiData.weather[0];
-                const iconUrl =`<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">`;
-                 //set DOM elements 
-                temperatureDegree.textContent = temp;
-                temperatureFeelsLike.textContent = feels_like;
-                locationTimezone.textContent = name;
-                countryName.textContent = country;
-                iconID.textContent = icon;
-                iconLink.innerHTML = iconUrl;                
-                imgDesc.textContent = description;               
-                humidityMeasure.textContent = humidity;
-            })
+                fetch(api) //fetches API data 
+                .then(apiResponse => 
+                    { //does line 12 once it receives the data
+                    return apiResponse.json();
+                    }) 
+                .then(apiData =>
+                {
+                    console.log(apiData);
+                    const {temp, feels_like, humidity}= apiData.main;
+                    const {name} = apiData;
+                    const{country} = apiData.sys;
+                    const {icon, description} = apiData.weather[0];
+                    const iconUrl =`<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">`;
+                    //set DOM elements 
+                    temperatureDegree.textContent = temp;
+                    temperatureFeelsLike.textContent = feels_like;
+                    locationTimezone.textContent = name;
+                    countryName.textContent = country;
+                    iconID.textContent = icon;
+                    iconLink.innerHTML = iconUrl;                
+                    imgDesc.textContent = description;               
+                    humidityMeasure.textContent = humidity;
+
+                    let realFahrenheit = (temp * (9/5)) + 32;
+                    let feelFahrenheight = (feels_like * (9/5)) + 32;
+
+                    //change temperature between Celcius and Fahrenheit
+                    degreeSection.addEventListener('click', () =>
+                    {
+                        if(degreeSectionSpan.textContent === '\u2103')
+                        {
+                            degreeSectionSpan.textContent = '\u2109'; //F
+                            temperatureDegree.textContent = realFahrenheit.toFixed(2);
+
+                            degreeFeelsSpan.textContent = '\u2109'; //F
+                            temperatureFeelsLike.textContent = feelFahrenheight.toFixed(2);
+                        }
+                        else{
+                            degreeSectionSpan.textContent = '\u2103';
+                            temperatureDegree.textContent = temp;
+
+                            degreeFeelsSpan.textContent = '\u2103';
+                            temperatureFeelsLike.textContent = feels_like;
+                        }
+                        
+                
+                    })
+
+                })
 
             const airQualityApi = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${weatherApiKey}`;
 
@@ -70,8 +101,6 @@ window.addEventListener('load', ()=>
                 })
         },
 
-
-    
         //fallback if user denies geolocation access
         function cityInput() 
         {
@@ -94,7 +123,7 @@ window.addEventListener('load', ()=>
                  const{country} = apiData.sys;
                  const {icon, description} = apiData.weather[0];
                  const iconUrl =`<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">`;
-                 const {lat,lon} = apiData.coord;
+                 
                   //set DOM elements 
                  temperatureDegree.textContent = temp;
                  temperatureFeelsLike.textContent = feels_like;
@@ -106,6 +135,30 @@ window.addEventListener('load', ()=>
                  humidityMeasure.textContent = humidity;
  
                  pm2_5Measure.textContent = "Enable location access.";
+
+                let realFahrenheit = (temp * (9/5)) + 32;
+                let feelFahrenheight = (feels_like * (9/5)) + 32;
+
+                degreeSection.addEventListener('click', () =>
+                {
+                    if(degreeSectionSpan.textContent === '\u2103')
+                    {
+                        degreeSectionSpan.textContent = '\u2109'; //F
+                        temperatureDegree.textContent = realFahrenheit.toFixed(2);
+
+                        degreeFeelsSpan.textContent = '\u2109'; //F
+                        temperatureFeelsLike.textContent = feelFahrenheight.toFixed(2);
+                    }
+                    else{
+                        degreeSectionSpan.textContent = '\u2103';
+                        temperatureDegree.textContent = temp;
+
+                        degreeFeelsSpan.textContent = '\u2103';
+                        temperatureFeelsLike.textContent = feels_like;
+                    }
+                        
+                
+                    })
                 })
  
         })
@@ -117,7 +170,7 @@ window.addEventListener('load', ()=>
 });
 
 function citySearch() 
-        {
+{
             let cityName = prompt('Enter City. \nEx: London or New york', "")
             let countryNameInput = prompt('Enter 2-letter country code.\nEx: GB or US', "");
             let temperatureFeelsLike = document.querySelector('.temperature-feelslike');
@@ -127,7 +180,6 @@ function citySearch()
             let iconID = document.querySelector('.icon-image')
             let iconLink = document.querySelector('.icon-display')
             let imgDesc = document.querySelector('.image-description')
-
             let pm2_5Measure = document.querySelector('.PM')
             let humidityMeasure = document.querySelector('.humidity')
          
@@ -159,12 +211,13 @@ function citySearch()
                  humidityMeasure.textContent = humidity;
  
                  pm2_5Measure.textContent = "Enable location access.";
-                 return feels_like;
+                 
                 })
-                console.log(feels_like);
-        };
+                
+};
 
-function locationCheck(){
+function locationCheck()
+{
     navigator.geolocation.watchPosition(function() {
         window.location.reload();
         console.log("nigga");
